4f551bfed1da8049b814d2cedbeddffc1393bacf
Redshark61 <tim.vannier@gmail.com>
2021-10-26 08:31:34 +0200

    first commit

---
+++ js/download.js
@@ -0,0 +1,5 @@
+// window.onload = () => {
+//     console.log("here");
+//     window.location.href =
+//         "https://github.com/Redshark61/Python-Island/archive/refs/heads/master.zip";
+// };
