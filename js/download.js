5d3fe65904909213a9e3d55d5a6d81995446b139
Redshark61 <tim.vannier@gmail.com>
2021-10-26 14:48:24 +0200

    not donwload file if it's a phone

--- js/download.js
+++ js/download.js
@@ -1,5 +1,7 @@
-window.onload = () => {
-    console.log("here");
-    window.location.href =
-        "https://github.com/Redshark61/Python-Island/archive/refs/heads/master.zip";
-};
+if (deviceType() == "desktop") {
+    window.onload = () => {
+        console.log("here");
+        window.location.href =
+            "https://github.com/Redshark61/Python-Island/archive/refs/heads/master.zip";
+    };
+}
